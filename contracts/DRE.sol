41862c273d944fe86a5cef77111f21900049cfd1
Ninja Solution <top1successfulman@gmail.com>
2023-07-24 22:08:40 +1000

    update

--- contracts/DRE.sol
+++ contracts/DRE.sol
@@ -21,23 +21,17 @@ contract DRE is ERC20, Ownable, Pausable  {
 
     address public vault;
 
-    uint256 public _maxTxAmount = 1000000 * 10 **_decimals;
-
-    IUniswapV2Router02 public immutable uniswapV2Router;
     address public immutable uniswapV2Pair;
     
 
     constructor(address _router, address _vault) ERC20("Dean Real Estate Governance", "DRE") {
  
         _mint(msg.sender, 100000000 * 10 ** _decimals);
-        // IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); /* mainnet */
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(_router); /* 0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45 */
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(_router);
         vault = _vault;
 
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
-
-        uniswapV2Router = _uniswapV2Router;
 
     }
 
